c329ae75153a3efbbcb1e67dbe5a7be81bedc848
samuelgvg2505 <115641099+samuelvg2505@users.noreply.github.com>
2024-11-12 22:26:38 +0000

    logging added
    Added the periodicity to the synchronization and logging to file and console

--- main.py
+++ main.py
@@ -2,42 +2,67 @@ import os
 from filecmp import dircmp
 import time
 import shutil
+import sys
+import logging
 
-src = 'C:/Users/samue/Documents/GitHub/test-task-veeam/src/'
-replica = 'C:/Users/samue/Documents/GitHub/test-task-veeam/replica/'
-
-dcmp = dircmp(src, replica)
-
-def print_diff_files(dcmp):
+def sync_files(dcmp, logger):
     for name in dcmp.left_only: #copy file to replica
 
         creation_date = time.ctime(os.path.getctime(dcmp.left+"/"+name))
         if(os.path.isdir(dcmp.left+"/"+name)):
-            print("New directory %s in src folder created at %s. Copying to replica" % (name, creation_date))
+            logger.info("New directory '%s' in src folder created at %s. Copying directory to replica" % (name, creation_date))
             shutil.copytree(dcmp.left+"/"+name, dcmp.right+"/"+name)
         else:
-            print("New file %s in src folder created at %s. Copying to replica" % (name, creation_date))
+            logger.info("New file '%s' in src folder created at %s. Copying file to replica" % (name, creation_date))
             shutil.copy2(dcmp.left+"/"+name, dcmp.right+"/"+name)
 
     for name in dcmp.right_only: #delete file from replica
 
-        creation_date = time.ctime(os.path.getctime(dcmp.right+"/"+name))
         if(os.path.isdir(dcmp.right+"/"+name)):
-            print("Directory %s in replica folder created at %s was deleted in src folder. Deleting the directory" % (name, creation_date))
+            logger.info("Directory '%s' was deleted in src folder. Deleting '%s' from replica" % (name, name))
             shutil.rmtree(dcmp.right+"/"+name)
         else:
-            print("New file %s in replica folder created at %s was deleted in src folder. Deleting the file" % (name, creation_date))
+            logger.info("File '%s' was deleted in src folder. Deleting '%s' from replica" % (name, name))
             os.remove(dcmp.right+"/"+name)
 
-    for name in dcmp.diff_files: #delete file from replica and copy new one
+    for name in dcmp.diff_files: #copy the modified file from src to replica
 
-        creation_date = time.ctime(os.path.getctime(dcmp.left+"/"+name))
         mod_date = time.ctime(os.path.getmtime(dcmp.left+"/"+name))
         if(not os.path.isdir(dcmp.left+"/"+name)):
-            print("File %s in src folder created at %s was modified at %s. Copying to replica" % (name, creation_date, mod_date))
+            logger.info("File '%s' in src folder was modified at %s. Copying file to replica" % (name, mod_date))
             shutil.copy2(dcmp.left+"/"+name, dcmp.right+"/"+name)
 
     for sub_dcmp in dcmp.subdirs.values():
-        print_diff_files(sub_dcmp)
+        sync_files(sub_dcmp, logger)
 
-print_diff_files(dcmp)
+def main():
+    src = sys.argv[1]
+    replica = sys.argv[2]
+    sync_period = float(sys.argv[3])
+
+    starttime = time.monotonic()
+
+    logger = logging.getLogger(__name__)
+    #file logger
+    logging.basicConfig(filename='sync.log', encoding='utf-8', format="{asctime}: [{levelname}] {message}", style="{", datefmt="%Y-%m-%d %H:%M", level=logging.INFO) 
+
+    #console logger
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_formatter = logging.Formatter('%(asctime)s: [%(levelname)s] %(message)s', datefmt="%Y-%m-%d %H:%M")
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    logger.info('Starting synchronization program...')
+
+    while True:
+        logger.info("Synchronizing....")
+        dcmp = dircmp(src, replica)
+        sync_files(dcmp, logger)
+        logger.info("Folders Synched!")
+        time.sleep(sync_period - ((time.monotonic() - starttime) % sync_period))
+
+
+if __name__ == '__main__':
+    main()
+
